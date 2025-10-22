72f8931159e9664149ecfc9062a43f5307c3b318
Danilo Mello <danilodemellow@gmail.com>
2025-10-21 22:02:11 -0300

    build: update vite configuration and ci, cd files

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://vida.danilabs.com.br/',
+  server: {
+    open: true,
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
