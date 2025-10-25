952fd4d82ef958eb0bf6093d290bc721c2508698
Danilo Mello <danilodemellow@gmail.com>
2025-10-25 14:28:41 -0300

    feat: install the vida-ui kit and use an button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import 'vida-ui/style.css';
+import { Button } from 'vida-ui';
+
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="w-full h-screen grid items-center justify-center">
+      <div className="text-center">
+        <h1>Hello World</h1>
+        <Button>Click Me</Button>
+      </div>
+    </div>
   );
 }
